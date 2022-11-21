ad8a1035633770a5146fa4c01bfa361e18bb9783
Nicolas Chipana <nicolas.chipana@acklenavenue.com>
2022-11-21 18:55:01 -0500

    refactored constant

--- client/src/config/constant.tsx
+++ client/src/config/constant.tsx
@@ -1,7 +1,2 @@
-let BACKEND_SERVER = null;
-if (process.env.REACT_APP_BACKEND_SERVER) {
-  BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
-} else {
-  BACKEND_SERVER = "http://localhost:4200/";
-}
-export const API_SERVER = BACKEND_SERVER;
+export const API_SERVER =
+  process.env.REACT_APP_BACKEND_SERVER ?? "http://localhost:4200/";
