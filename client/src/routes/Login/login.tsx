cbc7dcc365031ad5f2fa06876d696da5e1dbf107
Nicolas Chipana <nicolas.chipana@acklenavenue.com>
2022-11-21 20:41:18 -0500

    agregado método para hacer consultas en frontend

--- client/src/routes/Login/login.tsx
+++ client/src/routes/Login/login.tsx
@@ -1,20 +1,59 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { FC } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SudoPassLogo } from "../../components/Icons/sudoPassLogo";
+import { createUser, fetchDoesUserExist } from "./loginApi";
 import "./login.css";
 
 export const Login: FC = () => {
-  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  const {
+    loginWithRedirect,
+    isAuthenticated,
+    isLoading: isAuthLoading,
+    user,
+  } = useAuth0();
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
+
+  const [inputPassword, setInputPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { data: doesUserExist, isLoading: isUserLoading } = useQuery<boolean>(
+    ["user", user?.email],
+    () => fetchDoesUserExist(user?.email ?? ""),
+    { enabled: isAuthenticated }
+  );
+  const createUserMutation = useMutation(createUser, {
+    onSuccess: () => {
+      queryClient.refetchQueries(["user", user?.email]);
+    },
+  });
 
   const handleOnSignInWithGooglePressed = () => {
     loginWithRedirect();
   };
 
   const handleOnSignInWithMasterPasswordPressed = () => {
+    if (inputPassword !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    if (doesUserExist) {
+      alert("User already exists");
+      return;
+    } else {
+      createUserMutation.mutate({
+        email: user?.email ?? "",
+        password: inputPassword,
+        alias: user?.name ?? "",
+      });
+    }
     navigate("/dashboard");
   };
+
+  const isLoading = isAuthLoading || isUserLoading;
 
   return (
     <div className="login-container">
@@ -24,11 +63,29 @@ export const Login: FC = () => {
           Login with Google
         </button>
       )}
-      {isAuthenticated && !isLoading && (
+      {isAuthenticated && !isLoading && doesUserExist && (
         <div className="master-password-container">
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setInputPassword(e.target.value)}
+          />
           <button onClick={handleOnSignInWithMasterPasswordPressed}>
-            Log in
+            Log In
+          </button>
+        </div>
+      )}
+      {isAuthenticated && !isLoading && !doesUserExist && (
+        <div className="master-password-container">
+          <input
+            type="password"
+            onChange={(e) => setInputPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button onClick={handleOnSignInWithMasterPasswordPressed}>
+            Sign Up
           </button>
         </div>
       )}
---
+++ client/src/routes/Login/loginApi.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { API_SERVER } from "../../config/constant";
+
+export const fetchDoesUserExist = async (email: string) => {
+  const res = await axios.get(API_SERVER + "/auth/exist-user/" + email);
+  return res.data;
+};
+
+export const createUser = async (body: {
+  email: string;
+  alias: string;
+  password: string;
+}) => {
+  const res = await axios.post(API_SERVER + "/credentials", body);
+  return res.data;
+};
