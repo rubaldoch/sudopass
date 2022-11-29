b17db046e7dd7a3f1be98767e68a32ad99689ffb
Nicolas Chipana <nicolas.chipana@acklenavenue.com>
2022-11-28 19:40:31 -0500

    Validated master password on client

--- client/src/components/Header/header.tsx
+++ client/src/components/Header/header.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
 import "./header.css";
 import { SudoPassMiniLogo } from "../Icons/sudoPassMiniLogo";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Header: FC = () => {
+  const { logout } = useAuth0();
+
   return (
     <div className="header">
       <SudoPassMiniLogo />
       <div className="profile-button">
-        <AccountCircleIcon fontSize="large" />
+        <LogoutIcon fontSize="medium" onClick={() => logout()} />
       </div>
     </div>
   );
